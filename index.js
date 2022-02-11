044f4baf73ee4355aeb05557957f6f1291ea7ce7
Henrique Guazzelli Mendes <henrique@verifymyage.co.uk>
2022-02-10 22:17:15 -0300

    init

---
+++ index.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+app.post('/webhook', (req, res) => {
+    console.log("Recebendo WEBHOOK!!!!!!")
+    console.log(req.body);
+    console.log(req.query);
+
+    res.status(200).json();
+})
+
+app.listen(process.env.PORT, () => console.log("App running"))
